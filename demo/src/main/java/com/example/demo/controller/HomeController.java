53f286a9218e6f69d06a3e598505272dde75b72d
helloramen <onipanman888@gmail.com>
2024-08-28 01:58:19 +0900

    最低限の型組完成

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,5 +18,13 @@ public class HomeController{
     @GetMapping("/RouletteDinner_form")
     public String form(){
         return "form";
+    }
+
+    @PostMapping("/RouletteDinner_res")
+    public ModelAndView result(ModelAndView mav ,@RequestParam String area){
+        /*APIで何を取得できるかによってここの情報変わります。
+         * 地域だけは最低限ないといけないので実装してますが、まあAPI次第。*/
+        mav.setViewName("result");
+        return mav;
     }
 }
---
+++ demo/src/main/java/com/example/demo/model/GetRestaurantInfo.java
@@ -0,0 +1,5 @@
+package com.example.demo.model;
+
+public class GetRestaurantInfo{
+    //APIのDBから飲食店の情報をgetするクラス
+}
