fa9791ec71b63e755c6d87323e03eb40e392fef6
helloramen <onipanman888@gmail.com>
2024-08-27 23:48:32 +0900

    Create Spring boot project

---
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController{
+
+    @GetMapping("/RouletteDinner_home")    //一番最初に起動するやつ
+    public String start(){
+        return "home";
+    }
+
+    @GetMapping("/RouletteDinner_form")
+    public String form(){
+        return "form";
+    }
+    //こんにちは
+}
