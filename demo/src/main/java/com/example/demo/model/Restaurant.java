f00e6cecc0e5d86cc6c57f1ae6b66cd87b636cd6
helloramen <onipanman888@gmail.com>
2024-08-29 07:32:05 +0900

    Restaurant.javaのフィールド作成

--- demo/src/main/java/com/example/demo/model/Restaurant.java
+++ demo/src/main/java/com/example/demo/model/Restaurant.java
@@ -4,5 +4,10 @@ import lombok.Data;
 
 @Data
 public class Restaurant{
-    //レストランのクラス
+    private String areaPref;    //都道府県
+    private String areaMun;     //市区町村
+    private String areaOth;     //その他
+    private String genre;       //ジャンル
+    private int burgetMin;      //予算最小値
+    private int burgetMax;      //予算最大値
 }
