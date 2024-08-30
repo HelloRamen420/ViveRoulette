13d4a3b1eb4c59a7711c8e1173ad1e87750b4206
helloramen <onipanman888@gmail.com>
2024-08-31 02:58:59 +0900

    form.htmlの都道府県のボックス作成、HomeController.javaの空文字の処理

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController{
+    final static String NULLMESS="必要な情報を入力してください";
 
     @GetMapping("/RouletteDinner_home")    //一番最初に起動するやつ
     public String start(){
@@ -21,11 +22,19 @@ public class HomeController{
     }
 
     @PostMapping("/RouletteDinner_res")
-    public ModelAndView result(ModelAndView mav ,@RequestParam String area){
+    public ModelAndView result(ModelAndView mav ,@RequestParam String area,@RequestParam String pref){
 
+        if(area.isEmpty()){ //空文字の時の処理
+            mav.addObject("nullMes",NULLMESS);  //実際にはnullではなく空文字
+            mav.setViewName("form");
+            return mav;
+        }
+
+        mav.addObject("pref",pref);
+        mav.addObject("area",area);
+        mav.setViewName("result");
         /* APIで何を取得できるかによってここの情報変わります。
          * 地域だけは最低限ないといけないので実装してますが、まあAPI次第。*/
-        mav.setViewName("result");
         return mav;
     }
 }
